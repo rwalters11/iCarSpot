933727a6d8e04638e94c72c8f026299b0b1d267b
rwalters11 <rwalters.flyer@icloud.com>
2017-07-31 15:33:36 +0100

    Initial Commit

---
+++ CarSpotter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarSpotter
+//
+//  Created by Richard Walters on 31/07/2017.
+//  Copyright © 2017 Richard Walters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
